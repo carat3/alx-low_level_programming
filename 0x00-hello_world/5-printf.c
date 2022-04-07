83be3233efe5843f4b6eb992aa4c385c461f9aca
carat3 <medhanitabebe2@gmail.com>
2022-04-07 12:18:04 -0700

    hell yeah

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdu.h>
+/**
+ *main - entry point 
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art")
+return (0);
+}
