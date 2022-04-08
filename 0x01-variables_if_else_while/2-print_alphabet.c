dfe0a35967444c283af114a9dabdb995be6490c5
carat3 <medhanitabebe2@gmail.com>
2022-04-08 13:06:40 -0700

    oooh

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,18 @@
-#include <stdio.h>  
+#include <stdio.h>
  /**
 * main - entry point
 *
 *Return: Always 0 (success/correct)
 *
-*/ 
-int main()  
-{  
-    char ch = 'a';  
-  
-    printf("Lowercase English Alphabets:\n");  
-    while(ch <= 'z')  
-    {  
-        printf("%c ", ch);  
-        ch++;  
-    }  
-    printf("\n");  
-  
-    return 0;  
-}  
+*/
+int main(void)
+{
+    char ch = 'a';
+
+    while(ch <= 'z')
+{
+printf("%c ", ch);
+ch++;
+}
+return 0; 
+}
