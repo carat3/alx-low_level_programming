09d02e037b9025e9255419ad02071c8b3101492b
carat3 <medhanitabebe2@gmail.com>
2022-04-08 14:32:34 -0700

    plspls

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+*Return: Always 0 (success/correct)
+*
+*/
+int main(void)
+{
+int n;
+for (n == 0; n < 10; n++)
+printf(n);
+}
+printf("\n");
+
+return (0);
+}
