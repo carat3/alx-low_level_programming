9a58e738a896e129724b8566d66248f0d45398d8
carat3 <medhanitabebe2@gmail.com>
2022-04-08 12:42:29 -0700

    task 2 ooo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include < stdio.h >  
+ /**
+* main - entry point
+*
+*Return: Always 0 (success/correct)
+*
+*/ 
+int main()  
+{  
+    char ch = 'a';  
+  
+    printf("Lowercase English Alphabets:\n");  
+    while(ch <= 'z')  
+    {  
+        printf("%c ", ch);  
+        ch++;  
+    }  
+    printf("\n");  
+  
+    return 0;  
+}  
